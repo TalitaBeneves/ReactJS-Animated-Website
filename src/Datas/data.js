79ff79f829ece57365acfd06a495f0ad2deee243
Talita Beneves <talitabeneves15@gmail.com>
2021-11-07 04:18:54 -0300

    created Data

---
+++ src/Datas/data.js
@@ -0,0 +1,35 @@
+import MontBlanc from '../img/mont-blanc.png';
+import Everest from '../img/everest.png';
+import NangaParbat from '../img/nanga-parbat.png';
+import K2 from '../img/k2.png';
+
+const data = [
+    {
+        id: 1,
+        name: 'Mont Blanc ',
+        image: MontBlanc,
+        deaths: '1000 mortes'
+    },
+    {
+        id: 2,
+        name: 'Everest',
+        image: Everest,
+        deaths: '100 mortes'
+    },
+    {
+        id: 3,
+        name: 'Nanga Parbat',
+        image: NangaParbat,
+        deaths: '62 mortes'
+    },
+    {
+        id: 4,
+        name: 'K2',
+        image: K2,
+        deaths: '60 mortes'
+    },
+    
+    
+]
+
+export default data;
