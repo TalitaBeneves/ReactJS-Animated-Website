f26e33414336b538a74f7a80c264c7806e005401
Talita Beneves <talitabeneves15@gmail.com>
2021-11-07 01:08:56 -0300

    created folder CtaButton

---
+++ src/components/CtaButton/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CtaButtonStyled } from './styles.js';
+
+function CtaButton ({name}){
+  return (
+    <CtaButtonStyled>
+      {name}
+    </CtaButtonStyled>
+  )
+}
+
+export default CtaButton
---
+++ src/components/CtaButton/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const CtaButtonStyled = styled.a`
+  text-tranform: uppercase;
+  display: inline-block;
+  padding: 0.9rem 1.5rem;
+  border-radius: 8px;
+  font-size: 1.1rem;
+  font-weight: 500;
+  cursor: pointer;
+  transitionL all 0.4s ease-in-out;
+`;
